2578240e0076ef19d638725cad2f2d20252c986e
kobayashi <kobayashi.riku@classmethod.jp>
2023-05-31 21:46:25 +0900

    スクリプト作成

---
+++ cloudwatch_logs_delete.py
@@ -0,0 +1,33 @@
+import boto3
+from datetime import datetime, timedelta
+
+one_month_ago = datetime.now() - timedelta(days=30)
+one_month_ago_int = int(one_month_ago.timestamp() * 1000)
+
+logs = boto3.client('logs')
+
+log_groups = logs.describe_log_groups()
+
+for log_group in log_groups['logGroups']:
+    log_group_name = log_group['logGroupName']
+    log_stream = logs.describe_log_streams(
+            logGroupName=log_group_name,
+            orderBy='LastEventTime',
+            descending=True,
+            limit=1
+            )
+    last_event_timestamp = log_stream['logStreams'][0]['lastEventTimestamp']
+    
+    if last_event_timestamp < one_month_ago_int:
+        while True:
+            print('ロググループ' + log_group_name  + 'を削除します。')
+            delete_flag = input('問題無い場合はyキーを押下してください。削除しない場合はnキーを押下してください。>')
+            if delete_flag == 'y':
+                logs.delete_log_group(logGroupName=log_group_name)
+                print(log_group_name  + 'は削除されました。')
+                break
+            elif delete_flag == 'n':
+                print(log_group_name  + 'は削除されませんでした。')
+                break
+            else:
+                print("指定されたキー以外が入力されました。もう一度入力してください。")
